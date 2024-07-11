95e45417ce3ec24bac4774db0379a98261c44afb
Michael SMETS <michael.smets@studpro.fr>
2024-07-11 14:29:27 +0200

    ajout de tracket brevo

---
+++ src/lib/trackerbrevo.ts
@@ -0,0 +1,39 @@
+declare global {
+    interface Window {
+        sib: any;
+        sendinblue: any;
+    }
+}
+
+
+export function initBrevoTracker() {
+    window.sib = {
+        equeue: [],
+        client_key: "45x9ltm5vhwzp7wn8w7ci4r8"
+    };
+
+    window.sendinblue = {};
+    for (var j = ['track', 'identify', 'trackLink', 'page'], i = 0; i < j.length; i++) {
+        (function (k) {
+            window.sendinblue[k] = function () {
+                var arg = Array.prototype.slice.call(arguments);
+                (window.sib[k] || function () {
+                    var t: any = {};
+                    t[k] = arg;
+                    window.sib.equeue.push(t);
+                })(arg[0], arg[1], arg[2]);
+            };
+        })(j[i]);
+    }
+
+    var n = document.createElement("script"),
+        iscript = document.getElementsByTagName("script")[0];
+    n.type = "text/javascript";
+    n.id = "sendinblue-js";
+    n.async = true;
+    n.src = "https://sibautomation.com/sa.js?key=" + window.sib.client_key;
+    iscript.parentNode?.insertBefore(n, i);
+    window.sendinblue.page();
+}
+
+export { };
